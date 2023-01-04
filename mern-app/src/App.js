10067334a29cd5a6dfea22309bccd396d9e60fb0
Karitas <karitashall@gmail.com>
2023-01-04 20:36:20 +0000

    added script to json, vegetable display and keys

--- mern-app/src/App.js
+++ mern-app/src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [fruits, setFruits] = useState([]);
+  const [vegetables, setVegetables] = useState([]);
 
   useEffect(() => {
     const getFruits = async () => {
@@ -14,16 +15,31 @@ function App() {
       setFruits(data);
     };
 
+    const getVegetables = async () => {
+      //to be able to use async and await in useEffect we need to create an async function.
+      const response = await fetch("http://localhost:5001/vegetables"); //we fetch from our api server running on port 5001
+      const data = await response.json(); //we get the json data
+
+      setVegetables(data);
+    };
     getFruits();
+    getVegetables();
   }, []);
 
   return (
-    <div>
-      {fruits.map((fruit) => {
-        // The key for the fruit object is called "name"
-        return <p>{fruit.name}</p>;
-      })}
-    </div>
+    <>
+      <div>
+        {fruits.map((fruit) => {
+          // The key for the fruit object is called "name"
+          return <p key={fruit._id}>{fruit.name}</p>;
+        })}
+      </div>
+      <div>
+        {vegetables.map((vegetable) => {
+          return <p key={vegetable._id}>{vegetable.name}</p>;
+        })}
+      </div>
+    </>
   );
 }
 
